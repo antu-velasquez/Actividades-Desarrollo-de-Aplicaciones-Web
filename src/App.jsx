5d8080ab4806bc295161143dc1f434ec6085685e
antu_velasquez <antulio.velasquez@galileo.edu>
2026-04-28 21:54:08 -0600

    Archivos actividad 2.

--- src/App.jsx
+++ src/App.jsx
@@ -143,4 +143,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; 
--- src/store.js
+++ src/store.js
@@ -22,4 +22,4 @@ export const store = configureStore({
   reducer: {
     tasks: tasksSlice.reducer
   }
-});
+}); 
